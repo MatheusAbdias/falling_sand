cac628a01eb5e8a37d0a9c19fb94af4c577d0dfb
Matheus Abdias <matheusfabdias@gmail.com>
2024-08-31 16:57:35 -0300

    feat: :sparkles: update cell logic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math/rand"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -12,7 +13,7 @@ import (
 const (
 	screenWidth  = 400
 	screenHeight = 400
-	cellSize     = 10
+	cellSize     = 1
 )
 
 type Game struct {
@@ -33,7 +34,7 @@ func NewGame() *Game {
 	return &Game{
 		pixels:         make([]byte, screenWidth*screenHeight*4),
 		grid:           grid,
-		updateInterval: time.Millisecond * 100,
+		updateInterval: time.Millisecond * 5,
 	}
 }
 
@@ -45,8 +46,6 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 			if vy {
 				g.drawFilled(screen, px, py)
-			} else {
-				g.drawStoke(screen, px, py)
 			}
 		}
 	}
@@ -65,20 +64,6 @@ func (g *Game) drawFilled(screen *ebiten.Image, px, py float32) {
 	)
 }
 
-func (g *Game) drawStoke(screen *ebiten.Image, px, py float32) {
-	white := color.RGBA{0xFF, 0xFF, 0xFF, 0xFF}
-	vector.StrokeRect(
-		screen,
-		px,
-		py,
-		cellSize,
-		cellSize,
-		1,
-		white,
-		true,
-	)
-}
-
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
@@ -90,18 +75,52 @@ func (g *Game) Update() error {
 
 		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
 			mouseX, mouseY := ebiten.CursorPosition()
-			cellX := mouseX / cellSize
-			cellY := mouseY / cellSize
-			if cellX >= 0 && cellX < len(g.grid) && cellY >= 0 && cellY < len(g.grid[cellX]) {
-				g.grid[cellX][cellY] = true
+			mouseCol := mouseX / cellSize
+			mouseRow := mouseY / cellSize
+			matrix := 10
+			extend := matrix / 2
+
+			for x := -extend; x < extend; x++ {
+				for y := -extend; y < extend; y++ {
+					col := mouseCol + x
+					row := mouseRow + y
+					if col >= 0 && col < len(g.grid) && row >= 0 &&
+						row < len(g.grid[col]) {
+						g.grid[col][row] = true
+					}
+				}
 			}
 		}
 
 		for x := 0; x < len(g.grid); x++ {
 			for y := len(g.grid[x]) - 1; y >= 0; y-- {
-				if g.grid[x][y] && y < len(g.grid[x])-1 && !g.grid[x][y+1] {
-					g.grid[x][y+1] = true
-					g.grid[x][y] = false
+				currentCell := g.grid[x][y]
+				if currentCell {
+					if y < len(g.grid[x])-1 {
+						bellowCell := g.grid[x][y+1]
+						if !bellowCell {
+							g.grid[x][y] = false
+							g.grid[x][y+1] = true
+						} else {
+							pos := 1
+							if rand.Float64() < 0.5 {
+								pos = pos * -1
+							}
+							var bellowFCell bool
+							var bellowSCell bool
+							if x < len(g.grid)-1 && x > 0 {
+								bellowFCell = g.grid[x+pos][y+1]
+								bellowSCell = g.grid[x-pos][y+1]
+								if !bellowFCell {
+									g.grid[x][y] = false
+									g.grid[x+pos][y+1] = true
+								} else if !bellowSCell {
+									g.grid[x][y] = false
+									g.grid[x-pos][y+1] = true
+								}
+							}
+						}
+					}
 				}
 			}
 		}
